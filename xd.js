87225f54d0385e88014d6c8442e6f0a958428527
代小呆 <i@sell.loli.video>
2017-06-17 17:06:14 +0800

    Adding sliders & animations

--- xd.js
+++ xd.js
@@ -8,44 +8,49 @@ $.__proto__.sel = (element) => {
     return document.querySelectorAll(element);
 }
 
-$.__proto__.get = (url,callback) => {
-    // let x = new XMLHttpRequest();
-    // x.onreadystatechange = () => {
-    // if (x.readyState==4 && x.status==200) 
-    //    callback(x.responseText);
-    // }
-    // x.open('GET',url,true);
-    // x.send();
-
-    fetch(url).then(callback);
+$.__proto__.get = (url) => {
+    return new Promise((resolve) => {
+        let x = new XMLHttpRequest();
+        x.onreadystatechange = () => {
+        if (x.readyState==4 && x.status==200) 
+            resolve(x.responseText);
+        }
+        x.open('GET',url,true);
+        x.send();
+    });
+    //fetch(url).then(callback);
 }
 
 $.__proto__.load = (sel,url) => {
-    $.get(url,dat => $(sel).innerHTML = dat);
+    $.get(url).then((dat) => $(sel).innerHTML = dat);
 }
 
 $.__proto__.click = (selected,act) => {
     $.sel(selected).forEach((obj) => { obj.onclick = () => act(obj) });
 }
 
-var _ready_fns = [];
-var ready = false;
+$.__proto__.__priv = {
+    ready: false,
+    ready_fns: [],
+    ldr_fns: []
+};
 
 $.__proto__.fin = (fn) => {
-    if(ready) _ready_fns.push(fn);
+    if($.__priv.ready) $.__priv._ready_fns.push(fn);
     else fn();
 }
 
+
 window.onload = () => {
     console.log("pong!");
-    ready = true;
+    $.__priv.ready = true;
     $.sel("include").forEach((o) => {
-        $.get(o.getAttribute("href"),dat => o.innerHTML = dat);
+        $.get(o.getAttribute("href")).then(dat => o.innerHTML = dat);
     });
-    console.log(_ready_fns);
-    for(f in _ready_fns){
+    console.log($.__priv._ready_fns);
+    for(f in $.__priv._ready_fns){
         console.log(f);
-        _ready_fns[f]();
+        $.__priv._ready_fns[f]();
     }
-
+    
 }
